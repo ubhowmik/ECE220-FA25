dc5fdac4d8ca450d5d6eca3337125712800e9ff5
ubhowmik <46545523+ubhowmik@users.noreply.github.com>
2025-10-02 07:48:21 -0500

    Update matrix_2d.c

--- lec10/matrix_2d.c
+++ lec10/matrix_2d.c
@@ -8,7 +8,7 @@ int main()
 	int matrix[3][4]={{1,2,3,1},{4,5,6,4},{7, 8, 9,7}};
 	int des[4][3];
 	int x=0; int y=2;
-	print_2D_ptr(3,4,matrix);
+	print_2D_ptr(3,4,&matrix[0][0]);
 	printf("\n");		
 	matrix_change(x, y, matrix);
 	matrix_tr(matrix, des);
@@ -23,7 +23,7 @@ for(i=0; i<4; i++)
 	src[y][i]=temp;
 
 }
-print_2D_ptr(3,4,src);
+print_2D_ptr(3,4,&src[0][0]);
 }
 
 void matrix_tr(int src[][4], int des[][3])
@@ -36,7 +36,7 @@ for(j=0;j<4;j++)
 des[j][i]=src[i][j];
 }
 }
-print_2D_ptr(4,3,des);
+print_2D_ptr(4,3,&des[0][0]);
 }
 
 
